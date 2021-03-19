99c407a0a891cba9ff172c1256ab1dc8b710e94e
hlistikova <hlistikovaa@gmail.com>
2021-03-19 19:38:21 +0300

    Итоговый проект

--- js/order.js
+++ js/order.js
@@ -4,7 +4,7 @@ const orderClose = orderPopup.querySelector(".modal-close");
 const orderForm = orderPopup.querySelector(".order-form");
 const orderName = orderPopup.querySelector(".order-name");
 const orderEmail = orderPopup.querySelector(".order-email");
-const orderText = orderPopup.querySelector(".order-text");
+// const orderText = orderPopup.querySelector(".order-text");
 
 let isStorageSupport = true;
 let storage = "";
@@ -33,33 +33,18 @@ orderClose.addEventListener("click", function (evt) {
 	orderPopup.classList.remove("modal-error");
 });
 
-// orderForm.addEventListener("submit", function (evt) {
-// 	if (!orderName.value || !orderEmail.value || !orderText.value) {
-// 		evt.preventDefault();
-// 		orderPopup.classList.remove("modal-error");
-// 		orderPopup.offsetWidth = orderPopup.offsetWidth;
-// 		orderPopup.classList.add("modal-error");
-// 	} 
-// 	else {
-// 		if (isStorageSupport) {
-// 		localStorage.setItem("name", orderName.value);
-// 		localStorage.setItem("email", orderEmail.value);
-// 		}
-// 	}
-// });
- 
 orderForm.addEventListener("submit", function (evt) {
-	if (orderName.value && orderEmail.value && orderText.value) {
-		if (isStorageSupport) {
-		localStorage.setItem("name", orderName.value);
-		localStorage.setItem("email", orderEmail.value);
-		}
-	} 
-	else {
+	if (!orderName.value || !orderEmail.value) {
 		evt.preventDefault();
 		orderPopup.classList.remove("modal-error");
 		orderPopup.offsetWidth = orderPopup.offsetWidth;
 		orderPopup.classList.add("modal-error");
+	} 
+	else {
+		if (isStorageSupport) {
+		localStorage.setItem("name", orderName.value);
+		localStorage.setItem("email", orderEmail.value);
+		}
 	}
 });
   
@@ -74,3 +59,4 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
